48680927711f0f473c23923400d0e310bd6a9575
yogesh bansal <bansalindia850@gmail.com>
2025-01-28 22:48:20 +0530

    frontend updated

--- Frontend/src/Components/Opentask.jsx
+++ Frontend/src/Components/Opentask.jsx
@@ -8,11 +8,12 @@ const OpenTask = ({ setIsTaskOpen, taskId }) => {
   const [dueDate, setDueDate] = useState("");
   const [message, setMessage] = useState("");
   const [label, setLabel] = useState(false);
-  const [color,setColor] = useState("");
+  const [color, setColor] = useState("");
 
   const handlelabel = () => {
-    setLabel(true);
+    setLabel(!label);
   };
+  // console.log("taskid",taskId);
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
@@ -35,7 +36,6 @@ const OpenTask = ({ setIsTaskOpen, taskId }) => {
         dueDate: e.target.value,
       });
       setMessage(response.data.message || "Dates updated successfully!");
-      console.log("yugggggggggggggggggggt");
     } catch (error) {
       setMessage("Failed to update dates. Please try again.");
       console.error(error);
@@ -61,44 +61,52 @@ const OpenTask = ({ setIsTaskOpen, taskId }) => {
 
   const handleDeleteTask = async () => {
     try {
-      const response = await axios.delete(`http://localhost:5050/tasks/deletetask`,
+      const response = await axios.delete(
+        "http://localhost:5050/tasks/deletetask",
         {
           taskId,
         }
       );
-    
+
       if (response.status === 200) {
         setMessage("Task deleted successfully!");
-        setIsTaskOpen(false); 
+        setIsTaskOpen(false);
       } else {
         setMessage("Failed to delete task. Please try again.");
       }
     } catch (error) {
       setMessage(
-        error.response?.data?.message || "Error deleting task. Please try again."
+        error.response?.data?.message ||
+          "Error deleting task. Please try again."
       );
       console.error("Error details:", error);
     }
   };
 
-  const handleSetColor = async(label)=>{
-    try{
-      const response = await axios.patch(`http://localhost:5050/tasks/setcolor`,
-      {
-        taskId,label
-      }
+  const handleSetColor = async (label) => {
+    try {
+      const response = await axios.patch(
+        "http://localhost:5050/tasks/setcolor",
+        {
+          taskId,
+          label,
+        }
       );
-      console.log(taskId)
-    }
-    catch (error) {
+      console.log(response);
+    } catch (error) {
       console.error("Error details:", error);
     }
-  }
-  
+  };
+
   return (
-    <div>
-      <div style={styles.modal}>
-        <div style={styles.modalContent}>
+    <>
+      <div onClick={() => setIsTaskOpen(false)} style={styles.modal}>
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          style={styles.modalContent}
+        >
           <h3>Task Options</h3>
           <button
             style={styles.button}
@@ -131,23 +139,24 @@ const OpenTask = ({ setIsTaskOpen, taskId }) => {
           )}
           <button style={styles.button}>Attachment</button>
           <button style={styles.button}>Location</button>
-          <button style={styles.button} onClick={handlelabel}>Label</button>
-          <button style={styles.button} onClick={handleDeleteTask}>Delete Task</button>
-
+          <button style={styles.button} onClick={handlelabel}>
+            Label
+          </button>
           {label && (
             <div>
               <label>
                 Color
                 <input
                   type="color"
-                  onChange={(e) =>handleSetColor(e.target.value)}
+                  onChange={(e) => handleSetColor(e.target.value)}
                 />
               </label>
-
-              
             </div>
           )}
-          
+          <button style={styles.button} onClick={handleDeleteTask}>
+            Delete Task
+          </button>
+
           <button
             onClick={() => setIsTaskOpen(false)}
             style={styles.closeButton}
@@ -157,7 +166,7 @@ const OpenTask = ({ setIsTaskOpen, taskId }) => {
           {message && <p>{message}</p>}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -175,6 +184,7 @@ const styles = {
     zIndex: 1000,
   },
   modalContent: {
+    borderRadius: "10px solid",
     backgroundColor: "gray",
     padding: "20px",
     borderRadius: "8px",
